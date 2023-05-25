b823162a1840cba9adbee4cb40cf6ab58b061e64
Harold Mudosa <91553966+haroldmud@users.noreply.github.com>
2023-05-25 17:12:09 +0200

    Create Roman_Numerals_Decoder.js

---
+++ Roman_Numerals_Decoder.js
@@ -0,0 +1,23 @@
+function solution (roman) {
+  const romanObj={
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
+  let result = 0;
+  let previousValue = 0;
+  for(let i=roman.length-1; i >= 0; i--){
+    let value = romanObj[roman[i]];
+    if( value >= previousValue){
+      result += value;
+    }else{
+      result -= value;
+    }
+    previousValue = value;
+  }
+	return result;
+}
