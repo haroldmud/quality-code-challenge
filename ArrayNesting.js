0f40fcb62625e60d415c2bf6df0dfb9355f0af5b
Harold Mudosa <91553966+haroldmud@users.noreply.github.com>
2023-06-08 15:25:50 +0200

    Update ArrayNesting.js

--- ArrayNesting.js
+++ ArrayNesting.js
@@ -1,1 +1,14 @@
+const nestedArray = [1,2,[3,4,[5,6]],[7],]
 
+function handleNestedArray(nest){
+    const split = JSON.stringify(nest).split("");
+    const countLevel = []
+    for(i=0; i<split.length; i++){
+        if(split[i] === "["){
+            countLevel.push(split[i])
+        }
+    }
+    return countLevel.length;
+}
+
+console.log(handleNestedArray(nestedArray))
